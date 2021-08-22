ca5c421717088567f12f02e8eba0c3204b59f48e
LoTwT <709937065@qq.com>
2021-08-22 08:50:34 +0800

    openDialog modify

--- src/libs/dialog.js
+++ src/libs/dialog.js
@@ -1,6 +1,16 @@
 const { dialog } = require("electron")
 
-const showOpenDialog = (win) => dialog.showOpenDialog(win, {})
+const showOpenDialog = (win) => dialog.showOpenDialog(win, {
+    properties: [
+        "openFile",
+        "multiSelections",
+        "showHiddenFiles",
+    ],
+    filters: [
+        { name: "影音文件", extensions: ["mp4", "avi", "flv"] },
+        { name: "所有文件", extensions: ["*"] },
+    ]
+})
 
 module.exports = {
     showOpenDialog
